3313f036e340c9e9cbacd772d1f0d0ff90403ef6
Andrew <andrew.ku93@gmail.com>
2021-04-10 07:46:43 +0000

    add additional seed to rooms table

--- backend/db/seeds/seeds.sql
+++ backend/db/seeds/seeds.sql
@@ -1,7 +1,7 @@
 INSERT INTO
-users(firstName, lastName, description, is_owner, level, address, city, email, password)
+users(firstName, lastName, owner, level, address, city, email, password)
 
 VALUES
-    ('Mario', 'Bros', 'Early morning person with a love for the outdoors.', true, 3, '123 Nintendo Drive.', 'Imaginario', 'mario@nintendo.com', 'test'),
-    ('Luigi', 'Bros', 'Student looking for affordable housing. Experienced plumber with interests in picking mushrooms.', false, 3, '456 Nintendo Drive', 'Nintenbro', 'luigi@nintendo.com', 'test');
+    ('Mario', 'Bros', true, 3, '123 Nintendo Drive.', 'Imaginario', 'mario@nintendo.com', 'test'),
+    ('Luigi', 'Bros', false, 3, '456 Nintendo Drive', 'Nintenbro', 'luigi@nintendo.com', 'test');
     
