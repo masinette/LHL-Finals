a77126c3a8f9747093337f9bc3b35f41d011272e
mclmnop <m_charlebois@hotmail.com>
2021-04-15 07:29:25 -0400

    roommate card wip

--- frontend/src/components/roommates/RoommateCard.jsx
+++ frontend/src/components/roommates/RoommateCard.jsx
@@ -6,23 +6,33 @@ import './RoommateCard.scss';
 
 export default function RoommateCard(props) {
   const { user_id } = useParams();
+
   const roommateSearch = props.users.filter(user => {
     console.log(user.id)
     if (user.id === parseInt(user_id)){
+      //console.log("yoooo",user.firstname)
       return user
     }
   });
   const roommate = roommateSearch[0];
 
 
-  console.log("ROOMIIIIE", roommateSearch, "ID", user_id, "PROPS", typeof(props.users[2]) )
-  // const picture1publicId = `users/${roommate.id}/u${roommate.id}_p1`
+
+  console.log("ROOMIIIIE", roommate?.firstname, "ID", user_id, "PROPS", typeof(props.users[2]) )
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  const picture1publicId = `users/${user_id}/u${user_id}_p1`
   // const picture2publicId = `users/${roommate.id}/u${roommate.id}_p2`
   // const picture3publicId = `users/${roommate.id}/u${roommate.id}_p3`
-  // const picture4publicId = `users/${roommate.id}/u${roommate.id}_p4`
+  //const picture4publicId = `users/${user_id}/u${user_id}_p4`
 
   //console.log(picture1publicId)
-
+  let test="friends"
   return (
 
     <div className ="wholeComp">
@@ -32,10 +42,13 @@ export default function RoommateCard(props) {
       <div className="row" >
         <div className="column">
           <div className="profilePic">
-            <Image cloudName="Ds3bokefg" publicId="friends" alt="guitar">
+            <Image cloudName="Ds3bokefg"  publicId={picture1publicId}>
                   <Transformation width="150" height="300" crop="fill" />
             </Image>
-            <img src="https://res.cloudinary.com/ds3bokefg/image/upload/v1618432531/users/3/u3_p2.jpg"></img>
+           
+          
+    
+          
           </div>
 
 
@@ -49,40 +62,10 @@ export default function RoommateCard(props) {
       </div>
       <div className="row">
         <div className="column">
-          <Carousel className="carousel">
-          <Carousel.Item interval={1000}>
             <Image cloudName="Ds3bokefg" publicId="users/1/mark.jpg" alt="works!">
                 <Transformation width="150" height="300" crop="fill" />
             </Image>
-            <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item interval={500}>
-            <img
-              className="d-block w-100"
-              src="holder.js/800x400?text=Second slide&bg=282c34"
-              alt="Second slide"
-            />
-            <Carousel.Caption>
-              <h3>Second slide label</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="holder.js/800x400?text=Third slide&bg=20232a"
-              alt="Third slide"
-            />
-            <Carousel.Caption>
-              <h3>Third slide label</h3>
-              <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-        </Carousel>
-
+           
       </div>
       <div className="column">
       </div>
