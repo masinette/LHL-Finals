8cec35f73bfa66e34007f65e23840b0c1b1f0d8d
mclmnop <m_charlebois@hotmail.com>
2021-04-15 09:08:38 -0400

    basic css added, send form not working yet

--- frontend/src/components/roommates/RoommateCard.jsx
+++ frontend/src/components/roommates/RoommateCard.jsx
@@ -2,6 +2,7 @@ import { Carousel } from 'react-bootstrap';
 import { useParams} from 'react-router-dom';
 import {Image, Video, Transformation, CloudinaryContext} from 'cloudinary-react';
 import './RoommateCard.scss';
+import ContactRoommate from './contactRoommate';
 
 
 export default function RoommateCard(props) {
@@ -27,9 +28,9 @@ export default function RoommateCard(props) {
  
  
   const picture1publicId = `users/${user_id}/u${user_id}_p1`
-  // const picture2publicId = `users/${roommate.id}/u${roommate.id}_p2`
-  // const picture3publicId = `users/${roommate.id}/u${roommate.id}_p3`
-  //const picture4publicId = `users/${user_id}/u${user_id}_p4`
+  const picture2publicId = `users/${user_id}/u${user_id}_p2`
+  const picture3publicId = `users/${user_id}/u${user_id}_p3`
+  const picture4publicId = `users/${user_id}/u${user_id}_p4`
 
   //console.log(picture1publicId)
   let test="friends"
@@ -40,10 +41,11 @@ export default function RoommateCard(props) {
         <Transformation width="150" height="300" crop="fill" />
       </Image> */}
       <div className="row" >
-        <div className="column">
+        <div className="column alignJustCenter">
           <div className="profilePic">
             <Image cloudName="Ds3bokefg"  publicId={picture1publicId}>
-                  <Transformation width="150" height="300" crop="fill" />
+     {/*          <Transformation width="400" height="400" gravity="face" radius="max" crop="crop" /> */}
+              <Transformation width="200" crop="fill" />
             </Image>
            
           
@@ -54,20 +56,41 @@ export default function RoommateCard(props) {
 
 
         </div>
-        <div className="column">
-
-
+        <div className="column alignJustCenter">
+          <div className="descText">
+                <h1 className="name">{roommate?.firstname + roommate?.lastname}</h1>
+                <span>{roommate?.description}</span>
+          </div>
         </div>
 
       </div>
       <div className="row">
-        <div className="column">
-            <Image cloudName="Ds3bokefg" publicId="users/1/mark.jpg" alt="works!">
-                <Transformation width="150" height="300" crop="fill" />
-            </Image>
+  
+        <div className="column alignJustCenter">
+          <div name="morePictures">
+         
+              <Image cloudName="Ds3bokefg" publicId={picture2publicId} alt="picture2">
+                  <Transformation width="5" height="10"  />
+                  <Transformation width="200" crop="fill" />
+              </Image>
+              <Image cloudName="Ds3bokefg" publicId={picture3publicId} alt="picture2">
+                  <Transformation width="5" height="10"  />
+                  <Transformation width="200" crop="fill" />
+              </Image>
+              <Image cloudName="Ds3bokefg" publicId={picture4publicId} alt="picture3">
+                  <Transformation width="5" height="10"  />
+                  <Transformation width="200" crop="fill" />
+              </Image>
+    
+          </div>
            
       </div>
-      <div className="column">
+      <div className="column justCenter">
+        <div name="contact">
+          <ContactRoommate></ContactRoommate>
+          
+
+        </div>
       </div>
     </div>
   </div>
