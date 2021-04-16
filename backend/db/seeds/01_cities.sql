470bf709f2cd0675ad0dc9fbf3a5fc6e97bb26b9
masinette <masinette.ts@gmail.com>
2021-04-16 13:02:23 -0400

    last commit before merge

--- backend/db/seeds/01_cities.sql
+++ backend/db/seeds/01_cities.sql
@@ -2,7 +2,7 @@ INSERT INTO
 cities( name, latitude, longitude, description, image )
 
 VALUES
-  ('Toronto', 43.6532, 79.3932, 'Home of the CN Tower!', 'https://images.dailyhive.com/20180116075615/Untitled-design-213.jpg'),
-  ('Vancouver', 49.2827, 123.1207, 'Home of Stanley Park', 'https://www.hellobc.com/content/uploads/2018/03/2-6787-vancouver-skyline-2100x1000.jpg'),
-  ('Calgary', 51.0447, 114.0719, 'Home of the Calgary Stampede', 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Downtown_Calgary_2020-3.jpg/1200px-Downtown_Calgary_2020-3.jpg'),
-  ('Montreal', 45.5017, 73.5673, 'Home of Mont Royal!', 'https://i2.wp.com/wheelchairtravel.org/wp-content/uploads/2020/01/montreal-v2020-header.jpg?resize=1920%2C1024&ssl=1');
+  ('Toronto', 43.65364946930945, -79.38413301761516, 'Home of the CN Tower!', 'https://images.dailyhive.com/20180116075615/Untitled-design-213.jpg'),
+  ('Vancouver', 49.263785381404, -123.11428939457144, 'Home of Stanley Park!', 'https://www.hellobc.com/content/uploads/2018/03/2-6787-vancouver-skyline-2100x1000.jpg'),
+  ('Calgary', 51.04621314399029, -114.05741551737405, 'Home of the Calgary Stampede!', 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Downtown_Calgary_2020-3.jpg/1200px-Downtown_Calgary_2020-3.jpg'),
+  ('Montreal', 45.509114666140036, -73.5544183328989, 'Home of Mount Royal!', 'https://i2.wp.com/wheelchairtravel.org/wp-content/uploads/2020/01/montreal-v2020-header.jpg?resize=1920%2C1024&ssl=1');
