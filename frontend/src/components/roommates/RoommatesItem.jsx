6f34a87f0db38f36a0542df87132e4dd6a408701
mclmnop <m_charlebois@hotmail.com>
2021-04-17 20:17:02 -0400

    roommates pictures added

--- frontend/src/components/roommates/RoommatesItem.jsx
+++ frontend/src/components/roommates/RoommatesItem.jsx
@@ -1,5 +1,7 @@
 import { CardDeck, Card, Button, Col, Row, Container } from 'react-bootstrap';
 import { useHistory } from 'react-router-dom';
+import {Image, Video, Transformation, CloudinaryContext} from 'cloudinary-react';
+import './RoommatesItem.scss';
 
 export default function RoommatesItem(props) {
   const history = useHistory();
@@ -10,21 +12,30 @@ export default function RoommatesItem(props) {
     history.push(`/search/roommates/${id}`)
   }
   return (
-    <Container fluid onClick={() => redirect(props.id)}>
-      <Row> 
-        <Col >
-          <Card className="text-center">
-            <Card.Header>Listing # {props.id}</Card.Header>
-            <Card.Body>
-              <Card.Title onClick={() => redirect(props.id)}>{props.name}</Card.Title>
-              <Card.Text>
-                {props.description}
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer className="text-muted">Looking for a place in: {props.city}</Card.Footer>
-          </Card>
-        </Col>
-      </Row>
+    <Container>
+      <Container fluid onClick={() => redirect(props.id)}>
+        <Row> 
+          <Col >
+            <Card className="text-center">
+              <Card.Header>{props.name}</Card.Header>
+              <Card.Body>
+                <div className="roommatesSomething">
+                  <Image cloudName="Ds3bokefg"  publicId={`users/${props.id}/u${props.id}_p1.jpg`} className="d-block w-100" className="img">
+                    <Transformation width="200" height="200" crop="fill"/>
+                  </Image>
+                  <div className="roommatesSomething">
+                    <Card.Title onClick={() => redirect(props.id)}>{props.name}</Card.Title>
+                    <Card.Text>
+                      {props.description}
+                    </Card.Text>
+                  </div>
+                </div>
+              </Card.Body>
+              <Card.Footer className="text-muted">Looking for a place in: {props.city}</Card.Footer>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </Container>
   )
 }
