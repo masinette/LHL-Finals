1e402a35030c24df7e2308ab3729369a2019c603
mclmnop <m_charlebois@hotmail.com>
2021-04-18 19:32:20 -0400

    Agatha picture fixed

--- frontend/src/components/messages/ReplyForm.jsx
+++ frontend/src/components/messages/ReplyForm.jsx
@@ -37,7 +37,7 @@ export default function ReplyForm(props) {
       })
       .then((response)=> {
         console.log("RESPONSE",response.data)
-        history.push(`/messages/${user.id}`)
+        history.push(`/messages/`)
 
       }) 
       .then()
